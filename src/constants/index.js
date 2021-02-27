7f2866fbd17b2ed9e7a2b2ad2440dee085f57342
Ngo Duc Huy <17020798@vnu.edu.vn>
2021-02-27 18:20:37 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL || "https://viedu.herokuapp.com/api";
 export const ACCESS_TOKEN = "accessToken";
 
 export const PRESENTATIONS_LIST_SIZE = 10;
