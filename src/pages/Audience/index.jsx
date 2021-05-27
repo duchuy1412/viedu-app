e1b6b280021477e290a43dfa4d5a3975c3e4355c
HuyND <ngohuy99bn@gmail.com>
2021-05-28 00:28:31 +0700

    add qrcode

--- src/pages/Audience/index.jsx
+++ src/pages/Audience/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import NotFound from "common/NotFound";
 import Pin from "./screens/Pin";
+import PinWithURL from "./screens/Pin/PinWithURL";
 import Name from "./screens/Name";
 import Instruction from "./screens/Instruction";
 
@@ -13,6 +14,7 @@ function Audience(props) {
   return (
     <Switch>
       <Route exact path={match.url} component={Pin} />
+      <Route exact path={`${match.url}/pin/:pinCode`} component={PinWithURL} />
       <Route path={`${match.url}/name`} component={Name} />
       <Route path={`${match.url}/lobby`} component={Instruction} />
 
